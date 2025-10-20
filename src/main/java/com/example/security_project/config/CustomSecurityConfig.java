2cefd7398509fa1a4dccb5e6196c5a661198b6d0
hyoungwonkang <hwplus@gmail.com>
2025-10-20 23:59:16 +0900

    cors설정 configurationSource 메서드 호출 공부

--- src/main/java/com/example/security_project/config/CustomSecurityConfig.java
+++ src/main/java/com/example/security_project/config/CustomSecurityConfig.java
@@ -20,58 +20,84 @@ import com.example.security_project.security.handler.ApiAuthenticationSuccessHan
 import lombok.extern.slf4j.Slf4j;
 
 // Java Config class
-@Slf4j
-@EnableWebSecurity
-@Configuration
-public class CustomSecurityConfig {
+// @Slf4j
+// @EnableWebSecurity
+// @Configuration
+// public class CustomSecurityConfig {
     
+//     @Bean
+//     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//         log.info("------------------------------------------ SecurityFilterChain");
+        
+//         //1. CORS 설정 활성화
+//         http.cors(httpSecurityCorsConfigurer -> {
+//             httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()); // DI(의존성 주입) -> CORS 설정 메서드 호출
+//         });
+
+//         //2. 세션 비활성화: 클라이언트에서 오는 Session 저장 안하기
+//         http.sessionManagement(sessionConfig -> {
+//             sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//         });
+
+//         //3. CSRF 비활성화
+//         http.csrf(csrf -> csrf.disable());
+
+//         // 폼 기반 로그인 요청 처리
+//         http.formLogin(config -> {
+//             config.loginProcessingUrl("/api/v1/member/login");
+//             config.usernameParameter("email");
+//             // config.passwordParameter("password");
+//             config.successHandler(new ApiAuthenticationSuccessHandler());
+//             config.failureHandler(new ApiAuthenticationFailureHandler());
+//         });
+
+//         return http.build();
+//     }
+
+//     @Bean
+//     public PasswordEncoder passwordEncoder() {
+//         // 패스워드 암호화 (BCrypt 해싱 알고리즘)
+//         return new BCryptPasswordEncoder();
+//     }
+
+//     // CORS 설정 활성화
+//     @Bean
+//     public CorsConfigurationSource corsConfigurationSource() {
+
+//         CorsConfiguration configuration = new CorsConfiguration();
+//         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+//         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+//         // configuration.setAllowCredentials(true);
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+
+//         return source;
+//     }
+// }
+
+// java config class
+@Configuration
+@EnableWebSecurity
+public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        log.info("------------------------------------------ SecurityFilterChain");
-        
-        //1. CORS 설정 활성화
         http.cors(httpSecurityCorsConfigurer -> {
-            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()); // DI(의존성 주입)
-        });
-
-        //2. 세션 비활성화: 클라이언트에서 오는 Session 저장 안하기
-        http.sessionManagement(sessionConfig -> {
-            sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        });
-
-        //3. CSRF 비활성화
-        http.csrf(csrf -> csrf.disable());
-
-        // 폼 기반 로그인 요청 처리
-        http.formLogin(config -> {
-            config.loginProcessingUrl("/api/v1/member/login");
-            config.usernameParameter("email");
-            // config.passwordParameter("password");
-            config.successHandler(new ApiAuthenticationSuccessHandler());
-            config.failureHandler(new ApiAuthenticationFailureHandler());
+            httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource());
         });
 
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        // 패스워드 암호화 (BCrypt 해싱 알고리즘)
-        return new BCryptPasswordEncoder();
-    }
-
-    // CORS 설정 활성화
-    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-        // configuration.setAllowCredentials(true);
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization","Cache-Control","Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        source.registerCorsConfiguration("/**", configuration);
 
         return source;
     }
