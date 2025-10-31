766eeb6034f3cfa3183f4048d439de40f0f3c190
hyoungwonkang <hwplus@gmail.com>
2025-10-31 17:48:17 +0900

    시큐리티 공부

--- src/main/java/com/example/security_project/dto/MemberDto.java
+++ src/main/java/com/example/security_project/dto/MemberDto.java
@@ -6,68 +6,24 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
-import com.example.security_project.domain.MemberRole;
-
-import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-// import java.util.ArrayList;
-// import java.util.HashMap;
-// import java.util.List;
-// import java.util.Map;
-// import java.util.stream.Collectors;
-
-// import org.springframework.security.core.userdetails.User;
-
-// import lombok.Getter;
-// import lombok.Setter;
-// import lombok.ToString;
-
-// import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-// @ToString
-// @Getter @Setter
-// public class MemberDto extends User {
-    
-//     private String email;
-//     private String password;
-//     private String nickName;
-//     private List<String> roleNames = new ArrayList<>();
-
-//     // constructor method
-//     public MemberDto(String email, String password, String nickName, List<String> roleNames) {
-//         super(email, password,
-//             roleNames.stream().map(roleName -> new SimpleGrantedAuthority("ROLE_" + roleName)).collect(Collectors.toList()));
-//         this.email = email;
-//         this.password = password;
-//         this.nickName = nickName;
-//         this.roleNames = roleNames;
-//     }
-
-//     // 클레임
-//     public Map<String, Object> getClaims() {
-//         Map<String, Object> map = new HashMap<>();
-//         map.put("email", email);
-//         map.put("password", password);
-//         map.put("nickName", nickName);
-//         map.put("roleNames", roleNames);
-//         return map;
-//     }
-// }
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 @ToString
 @Getter @Setter
 public class MemberDto extends User {
+    
     private String email;
     private String password;
     private String nickName;
     private List<String> roleNames = new ArrayList<>();
 
+    // constructor method
     public MemberDto(String email, String password, String nickName, List<String> roleNames) {
         super(email, password,
             roleNames.stream().map(roleName -> new SimpleGrantedAuthority("ROLE_" + roleName)).collect(Collectors.toList()));
@@ -77,6 +33,7 @@ public class MemberDto extends User {
         this.roleNames = roleNames;
     }
 
+    // 클레임
     public Map<String, Object> getClaims() {
         Map<String, Object> map = new HashMap<>();
         map.put("email", email);
